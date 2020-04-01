d24dfdb7dd246654052a3c1599d9cbc835bfb45c
Edu7216 <721615@unizar.es>
2020-04-01 19:37:21 +0200

    Modificaciones en el repositorio de Gerente

--- src/main/java/com/LS/Dominio/Repositorio/GerenteRepository.java
+++ src/main/java/com/LS/Dominio/Repositorio/GerenteRepository.java
@@ -1,3 +1,15 @@
+/*
+ * GerenteRepository.java 1.0 01/04/2020
+ */
+
+/**
+ * Esta interfaz es el repositorio de Gerente
+ * utilizado para mantener persistencia de la entidad Gerente
+ *
+ * @author Eduardo Gimeno
+ * @version 1.0, 01/04/2020
+ */
+
 package com.LS.Dominio.Repositorio;
 
 import com.LS.Dominio.Entidad.Gerente;
