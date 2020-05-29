4be8d5f68e36e43c79a4b89dc5d6101612a3c818
Gonzalo Berne Melguizo <gera1397@gmail.com>
2020-05-29 19:16:54 +0200

    FiltrarBusquedaEspacios con BusquedaDTO acabado

--- src/main/java/com/LS/Dominio/Servicio/FiltrarBusquedaEspacios.java
+++ src/main/java/com/LS/Dominio/Servicio/FiltrarBusquedaEspacios.java
@@ -2,11 +2,16 @@ package com.LS.Dominio.Servicio;
 
 import DTO.BusquedaDTO;
 import com.LS.Dominio.Entidad.Espacio;
+import com.LS.Dominio.ObjetoValor.Equipamiento;
+import com.LS.Dominio.Parser.ObjetosValorParser;
 import com.LS.Dominio.Repositorio.EspacioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FiltrarBusquedaEspacios {
@@ -14,40 +19,76 @@ public class FiltrarBusquedaEspacios {
     @Autowired
     private EspacioRepository espacioRepository;
 
+    @Autowired
+    ObjetosValorParser objetosValorParser;
+
     public Collection<Espacio> filtrar (BusquedaDTO busquedaDTO) {
+        List<Espacio> espacios = new ArrayList<>();
         if (busquedaDTO.getEdificio().equals("null")) {
             if (busquedaDTO.getTipoEspacio().equals("null")) {
                 if (busquedaDTO.getCapacidad() < 0) {
-                    return espacioRepository.obtenerTodos();
+                    espacios = espacioRepository.obtenerTodos();
                 } else {
-                    return espacioRepository.findByCapacidadMinima(busquedaDTO.getCapacidad());
+                    espacios = espacioRepository.findByCapacidadMinima(busquedaDTO.getCapacidad());
                 }
             } else {
                 if (busquedaDTO.getCapacidad() < 0) {
-                    return espacioRepository.findByTipo(busquedaDTO.getTipoEspacio());
+                    espacios = espacioRepository.findByTipo(busquedaDTO.getTipoEspacio());
                 } else {
-                    return espacioRepository.findByTipoAndCapacidadMinima(
+                    espacios = espacioRepository.findByTipoAndCapacidadMinima(
                             busquedaDTO.getTipoEspacio(), busquedaDTO.getCapacidad());
                 }
             }
         } else {
             if (busquedaDTO.getTipoEspacio().equals("null")) {
                 if (busquedaDTO.getCapacidad() < 0) {
-                    return espacioRepository.findByUbicacionEdificio(busquedaDTO.getEdificio());
+                    espacios = espacioRepository.findByUbicacionEdificio(busquedaDTO.getEdificio());
                 } else {
-                    return espacioRepository.findByUbicacionEdificioAndCapacidadMinima(
+                    espacios = espacioRepository.findByUbicacionEdificioAndCapacidadMinima(
                             busquedaDTO.getEdificio(), busquedaDTO.getCapacidad());
                 }
             } else {
                 if (busquedaDTO.getCapacidad() < 0) {
-                    return espacioRepository.findByUbicacionEdificioAndTipo(
+                    espacios = espacioRepository.findByUbicacionEdificioAndTipo(
                             busquedaDTO.getEdificio(), busquedaDTO.getTipoEspacio());
                 } else {
-                    return espacioRepository.findByUbicacionEdificioAndTipoAndCapacidadMinima(
+                    espacios = espacioRepository.findByUbicacionEdificioAndTipoAndCapacidadMinima(
                             busquedaDTO.getEdificio(), busquedaDTO.getTipoEspacio(), busquedaDTO.getCapacidad());
                 }
             }
         }
+        if (!busquedaDTO.getEquipamiento().isEmpty()) {
+            espacios = filtrarPorEquipamiento(espacios,
+                    busquedaDTO.getEquipamiento()
+                            .stream()
+                            .map(objetosValorParser::equipamientoDTOAOV)
+                            .collect(Collectors.toList()));
+        }
+        return espacios;
+    }
+
+    public List<Espacio> filtrarPorEquipamiento(List<Espacio> espacios, List<Equipamiento> equipamientos) {
+        List<Espacio> espaciosFiltrados = new ArrayList<>();
+        for (Espacio espacio: espacios) {
+            boolean correcto = true;
+            for (Equipamiento equipamiento: equipamientos) {
+                boolean tieneEquipamiento = false;
+                for (Equipamiento e: espacio.getEquipamiento()) {
+                    if (e.getTipo() == equipamiento.getTipo() && e.getCantidad() >= equipamiento.getCantidad()) {
+                        tieneEquipamiento = true;
+                        break;
+                    }
+                }
+                if (!tieneEquipamiento) {
+                    correcto = false;
+                    break;
+                }
+            }
+            if (correcto) {
+                espaciosFiltrados.add(espacio);
+            }
+        }
+        return espaciosFiltrados;
     }
 
 }
